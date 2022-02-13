3dfde9701bc8b76596d9a3af9cfecee14d8d8023
BaOai305 <orehx01@gmail.com>
2022-02-13 17:18:39 +0700

    Update Controller.java

--- src/java/controller/Controller.java
+++ src/java/controller/Controller.java
@@ -31,16 +31,6 @@ public class Controller extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet Controller</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet Controller at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
         }
     }
 

e2c1b17838cf43ac069376f655ca382dc2cf0f0e
BaOai305 <orehx01@gmail.com>
2022-02-14 12:16:18 +0700

    Create Seller.java

---
+++ src/java/model/Seller.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Phamb
+ */
+public class Seller {
+    private String selName, selAddress;
+    private int selPhone;
+
+    public String getSelName() {
+        return selName;
+    }
+
+    public void setSelName(String selName) {
+        this.selName = selName;
+    }
+
+    public String getSelAddress() {
+        return selAddress;
+    }
+
+    public void setSelAddress(String selAddress) {
+        this.selAddress = selAddress;
+    }
+
+    public int getSelPhone() {
+        return selPhone;
+    }
+
+    public void setSelPhone(int selPhone) {
+        this.selPhone = selPhone;
+    }
+    
+}

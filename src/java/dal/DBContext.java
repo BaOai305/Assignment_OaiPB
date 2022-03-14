60402806f38fcb31fc6355dd23d4f96c3709cc8a
BaOai305 <orehx01@gmail.com>
2022-03-14 23:43:43 +0700

    Update DBContext.java

--- src/java/dal/DBContext.java
+++ src/java/dal/DBContext.java
@@ -35,7 +35,6 @@ public class DBContext {
     public Connection getConnection() throws Exception {
         String user = "sa";
         String pass = "123456";
-        
         String url = "jdbc:sqlserver://BAOAI:1433;databaseName=Assignment";
         Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
         return DriverManager.getConnection(url, user, pass);
